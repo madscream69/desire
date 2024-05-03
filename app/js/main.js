fd75ffcc47c4ec0c8d4b7d167994eec5eef33816
Dolbezhnik <krendelalenkin@gmail.com>
2024-05-03 20:25:46 +0300

    section with slider is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,26 @@
-'use strict'
-let burger = document.querySelector('.header__btn'),
-    rightside_menu = document.querySelector('.rightside-menu'),
-    menu_close = document.querySelector('.rightside-menu__close');
-console.log(menu_close);
-burger.addEventListener('click', ()=>{
-    rightside_menu.classList.remove('rightside-menu--close')
-});
-menu_close.addEventListener('click', ()=>{
-    rightside_menu.classList.add('rightside-menu--close')
-});
-// $(function () {
-//     $('.header-btn').on('click', () =>{
-//         $('rightside-menu').removeClass('rightside-menu--close')
-//     });
-//     $('rightside-menu__close').on('click', () =>{
-//         $('rightside-menu').addClass('rightside-menu--close')
-//     });
-// })
+$(function () {
+    $('.header__btn').on('click', function (){
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function (){
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+})
+// 'use strict'
+// let burger = document.querySelector('.header__btn'),
+//     rightside_menu = document.querySelector('.rightside-menu'),
+//     menu_close = document.querySelector('.rightside-menu__close');
+
+// burger.addEventListener('click', ()=>{
+//     rightside_menu.classList.remove('rightside-menu--close')
+// });
+// menu_close.addEventListener('click', ()=>{
+//     rightside_menu.classList.add('rightside-menu--close')
+// });
+
