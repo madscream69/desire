d670b4847c0a457c4a96ad2a2a78f9090e45eaa5
Vladimir-MaibookM <krendelalenkin@gmail.com>
2024-05-10 19:22:01 +0300

    active statment for the headers elemnts is done

--- app/js/main.js
+++ app/js/main.js
@@ -17,6 +17,34 @@ $(function () {
         }
     });
 })
+console.log(window.location.href);
+// this consruction for the active class to headers elements
+if ((window.location.href).includes('about')) {
+    const about = document.querySelectorAll('.menu__list-link');
+    console.log(about);
+    for (let i = 0; i < about.length; i++) {
+        if (about[i].innerHTML.includes('About')) {
+            console.log('yes');
+            about[i].classList.add('menu__list-link--active');
+            break
+        }
+        else if(about[i].innerHTML.includes('Gallery')){
+            about[i].classList.add('menu__list-link--active');
+            break
+        }
+        else if(about[i].innerHTML.includes('Blog')){
+            about[i].classList.add('menu__list-link--active');
+            break
+        }
+        else if(about[i].innerHTML.includes('Contact')){
+            about[i].classList.add('menu__list-link--active');
+            break
+        }
+        else{
+            about[i].classList.remove('menu__list-link--active');
+        }
+    }
+}
 // 'use strict'
 // let burger = document.querySelector('.header__btn'),
 //     rightside_menu = document.querySelector('.rightside-menu'),
