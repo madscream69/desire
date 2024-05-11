a3059374104582f949fafe521a094751b5e55f77
Dolbezhnik <krendelalenkin@gmail.com>
2024-05-11 21:17:26 +0300

    section about prev+content+popup is done

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ function sprite() {
 }
 
 function scripts() {
-    return src(['node_modules/jquery/dist/jquery.js', 'node_modules/slick-carousel/slick/slick.js', 'node_modules/mixitup/dist/mixitup.js', 'app/js/main.js'])
+    return src(['node_modules/jquery/dist/jquery.js', 'node_modules/slick-carousel/slick/slick.js', 'node_modules/mixitup/dist/mixitup.js', 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js', 'app/js/main.js'])
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(dest('app/js'))
